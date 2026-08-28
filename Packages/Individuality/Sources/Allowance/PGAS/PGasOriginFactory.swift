0a1f47c8bb822c094bcd11e2a4b29e5832a795da
Sergey Kononenko <sergey.kononenko@novasama.io>
2026-08-28 13:45:11 +0200

    Fix PGAS ring roots subscription for 3-key NMap storage

--- Packages/Individuality/Sources/Allowance/PGAS/PGasOriginFactory.swift
+++ Packages/Individuality/Sources/Allowance/PGAS/PGasOriginFactory.swift
@@ -3,6 +3,7 @@ import ExtrinsicService
 import Foundation
 import KeyDerivation
 import SubstrateSdk
+import SubstrateStorageQuery
 import SubstrateStorageSubscription
 import StructuredConcurrency
 import SubstrateSdkExt
@@ -27,13 +28,16 @@ public final class PGasOriginFactory {
 
     private let keyResolver: BandersnatchKeyResolving
     private let chainRegistry: ChainResourceProtocol
+    private let storageRequestFactory: StorageRequestFactoryProtocol
 
     public init(
         keyResolver: BandersnatchKeyResolving,
-        chainRegistry: ChainResourceProtocol
+        chainRegistry: ChainResourceProtocol,
+        storageRequestFactory: StorageRequestFactoryProtocol
     ) {
         self.keyResolver = keyResolver
         self.chainRegistry = chainRegistry
+        self.storageRequestFactory = storageRequestFactory
     }
 }
 
@@ -137,6 +141,24 @@ private extension PGasOriginFactory {
         return revisionValue ?? 0
     }
 
+    func fetchCurrentGeneration(
+        connection: JSONRPCEngine,
+        runtimeService: RuntimeCodingServiceProtocol
+    ) async throws -> MembersSubscriberPallet.Generation {
+        let codingFactory = try await runtimeService.fetchCoderFactoryOperation().asyncExecute()
+        let generationPath = MembersSubscriberPallet.Storage.currentGeneration()
+        let response: StorageResponse<StringCodable<MembersSubscriberPallet.Generation>> =
+            try await storageRequestFactory
+                .queryItem(
+                    engine: connection,
+                    factory: { codingFactory },
+                    storagePath: generationPath
+                )
+                .asyncExecute()
+
+        return response.value?.wrappedValue ?? 0
+    }
+
     func awaitRingRevision(
         submissionChainId: ChainId,
         collectionId: Data,
@@ -146,14 +168,18 @@ private extension PGasOriginFactory {
         let connection = try chainRegistry.getRpcConnectionOrError(for: submissionChainId)
         let runtimeService = try chainRegistry.getRuntimeCodingServiceOrError(for: submissionChainId)
 
-        let request = DoubleMapSubscriptionRequest(
+        let generation = try await fetchCurrentGeneration(connection: connection, runtimeService: runtimeService)
+
+        let request = NMapSubscriptionRequest(
             storagePath: MembersSubscriberPallet.Storage.ringRoots(),
             localKey: "",
             keyParamClosure: {
-                (BytesCodable(wrappedValue: collectionId), StringCodable(wrappedValue: ringIndex))
-            },
-            param1Encoder: nil,
-            param2Encoder: nil
+                MembersSubscriberPallet.RingRootsKey(
+                    generation: generation,
+                    collectionId: collectionId,
+                    ringIndex: ringIndex
+                )
+            }
         )
 
         let batchRequest = BatchStorageSubscriptionRequest(
